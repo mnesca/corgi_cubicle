ca4088b67685fb86d33e53b5bafd79003026238b
Marcello Nesca <umnescam@myumanitoba.ca>
2019-07-07 16:32:32 -0500

    July 7 2019  changes

--- Packages.R
+++ Packages.R
@@ -5,3 +5,8 @@ install.packages("here")
 ## A good reference is this github page <https://github.com/jennybc/here_here>
 
 install.packages("rmarkdown")
+
+install.packages("readr")
+## *readr package* easily reads files such as CSV -- not sure if this is part of the tidyverse
+## but overall it makes coding much easier
+## ref: <https://cran.r-project.org/web/packages/readr/readr.pdf>
