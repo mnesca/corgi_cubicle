6733631ac29fe9b1455fa0891891feacf23e8d8e
Marcello Nesca <umnescam@myumanitoba.ca>
2019-07-07 15:36:13 -0500

    This is where issues will be stored and see if they are solved.

---
+++ Issues.R
@@ -0,0 +1,10 @@
+
+#########################
+## Problems and Issues ##
+#########################
+
+# 1) how to save R Markdown files on github.
+#  SOLVED #
+
+
+
--- Packages.R
+++ Packages.R
@@ -1,4 +1,7 @@
 ## Packages to Install if on different computers
 
 install.packages("here")
+## *Here package* is supposed to find the project path that you are working off of
+## A good reference is this github page <https://github.com/jennybc/here_here>
+
 install.packages("rmarkdown")
