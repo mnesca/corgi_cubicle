6536a2adbfe45544d9ddd77a943538562c622ed9
Marcello Nesca <umnescam@myumanitoba.ca>
2019-12-14 10:38:01 -0600

    deleting filess

--- Extra old files/testing to see if this works.R
+++
@@ -1,8 +0,0 @@
-## this is just a test file
-## Please make this work!
-
-## testing again on my desktop
-library(here)
-here()
-
-# testing to see if you really need a commit comment to push the file through.
